e7181b8a6c108c81a607137f220ffe4538f2c524
Lisa Cook <jemilox@gmail.com>
2016-09-18 14:15:44 -0500

    functional

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,7 +1,136 @@
 console.log('sourced');
+var toDos = [];
 
+//all button clicks in doc ready
 $(document).ready(function(){
-  console.log('doc ready');
+  //console.log('doc ready');
+  //get all to dos in db and display
+  getAll();
+  //get onlick from insert todo
+  $('.addToDoButton').on('click', function () {
+    //console.log('in addToDoButton');
+    //add input box
+    var submitToDo = '<input id="newToDoIn" type="text" placeholder="Add to do message"><button id="submitToDoButton">Submit</button>';
+    $('.addInput').html(submitToDo);
+    on = false;
+
+  });//end .addToDoButton
+
+  //submit click button
+  $('.addToDo').on('click', '#submitToDoButton', function () {
+    //console.log('in addToDo');
+    //run through add to server function
+    addToDoText();
+  });
+
+  $('.currentToDoList').on('click', '#done', function () {
+    //console.log('in finishedToDoList button');
+    //get data attribute for which todo needs to be updated to not active
+    var whichToDo = $(this).attr('data');
+    console.log('whichToDo', whichToDo);
+    //run updateActive
+    updateActive(whichToDo);
+  });
+
+  $('.notepad').on('click', "#delete", function () {
+    //console.log('in delete button');
+    var whichToDelete = $(this).attr('data');
+    //run delete function
+    deleteToDo(whichToDelete);
+  });
+});//end docReady
 
 
-});
+//add new todo to table
+var addToDoText = function () {
+
+  var newToDo = $('#newToDoIn').val();
+  console.log('newToDo: ', newToDo);
+  //clearn addInput
+    $('.addInput').html('');
+  // create object for todo
+  var toDoToSend= {
+    toDo : newToDo,
+    active : true
+  }; // end object
+  console.log('toDoToSend', toDoToSend);
+  //ajax call new employee
+  $.ajax({
+    url: '/newtodotext',
+    type: 'POST',
+    data: toDoToSend,
+    success: function (data) {
+      console.log('ajax gets back:', data);
+
+      //getAll();
+      getAll();
+    }//end success
+  });//end ajax
+};
+
+
+//get all from todo table
+var getAll = function () {
+  $.ajax({
+      url: '/getalltodos',
+      type: 'GET',
+      success: function (data) {
+        console.log('getalltodos', data);
+        toDos = data;
+        console.log('toDos = ', toDos);
+        //displayAll
+        displayAll();
+      }//end success
+    });//end ajax getemployees
+};//end getAll
+
+//display all from table
+var displayAll = function () {
+  //clear both
+  $('#currentToDoList').html('');
+  $('#finishedToDoList').html('');
+  for (var i = 0; i < toDos.length; i++) {
+    if(toDos[i].active){
+      $('#currentToDoList').append('<li><button id="done" data="' + i + '">Done</button>' + toDos[i].todotext + '<button id="delete" data=' + i + '>x</button></li>');
+    }else{
+      $('#finishedToDoList').append('<li>' + toDos[i].todotext + '<button id="delete" data=' + i + '>x</button></li>');
+    }
+  }
+};//end displayAll
+
+//update todo from active to not active
+var updateActive = function (thisToDo) {
+  console.log('in updateActive');
+  var toDoToUpdate = {
+    updateToDoText: toDos[thisToDo].todotext
+  };//end toDoToUpdate object
+  //send to db
+  $.ajax({
+    url: '/updatetodo',
+    type: 'POST',
+    data: toDoToUpdate,
+    success: function (data) {
+      console.log('ajax gets back:', data);
+
+      //getAll();
+      getAll();
+    }//end success
+  });//end aja
+};//end updateActive
+
+//delete todo from table
+var deleteToDo = function (thisToDo) {
+  console.log('in deleteTodo');
+  var toDoToDelete = {
+    toDelete: toDos[thisToDo].todotext
+  };//end object
+  $.ajax({
+    url: '/deletetodo',
+    type: 'POST',
+    data: toDoToDelete,
+    success: function (data) {
+      console.log('ajax delete gets back', data);
+      getAll();
+    }//end success
+  });//end ajax
+};//end deleteToDo
--- server/app.js
+++ server/app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded( {extended: false});
 var portDecision = process.env.PORT || 3000;
-
-var count = 0;
+var pg = require('pg');
+var connectionString = 'postgress://localhost:5432/todoproject';
 
 app.listen( portDecision, function () {
   console.log("3000 is up!");
@@ -17,3 +17,95 @@ app.get('/', urlencodedParser, function (req, res) {
 });
 
 app.use(express.static('public'));
+
+app.post('/newtodotext', urlencodedParser, function (req, res) {
+  console.log('in .post newEmployee');
+  console.log('req.body', req.body);
+  //create variables from req
+  var toDo = req.body.toDo;
+  var active = req.body.active;
+  console.log('toDo and active:', toDo, active);
+  //connect to database
+  pg.connect( connectionString, function (err, client, done) {
+    if (err){
+      console.log(err);
+    }else {
+      console.log('connected to database');
+      var queryResults = client.query('INSERT INTO todolist(todotext, active) VALUES($1, $2)', [toDo, active]);
+    }//end else
+    queryResults.on('end', function () {
+      done();
+      res.send({success: true});
+    });
+  });//end pg connect
+});//end app.post
+
+app.get('/getalltodos', function (req, res) {
+  console.log('in /getalltodos');
+  pg.connect(connectionString, function (err, client, done) {
+    if (err){
+      console.log(err);
+    }else{
+      var allToDos = [];
+      var queryResults = client.query('SELECT * FROM todolist');
+      console.log(queryResults);
+      queryResults.on('row', function (row) {
+        allToDos.push(row);
+      });
+      console.log('alltoDos', allToDos[0]);
+      queryResults.on('end', function () {
+        done();
+         return res.json(allToDos);
+      });//end queryResults function
+    }//end else
+  });//end pg connect
+});//end app.get
+
+//update todo to inactive
+app.post('/updatetodo', urlencodedParser, function (req, res) {
+  console.log('in updatetodo');
+  console.log('req.body', req.body);
+  //create variables from req
+  // var updateToDo = req.body.updateToDoText;
+  var updateActiveWhich = req.body.updateToDoText;
+  console.log('updateActive:', updateActiveWhich);
+  //connect to database
+  pg.connect( connectionString, function (err, client, done) {
+    if (err){
+      console.log(err);
+    }else {
+      console.log('connected to database in update');
+      var queryResults = client.query('UPDATE todolist SET active=false WHERE todotext= $1', [updateActiveWhich]);
+
+    }//end else
+    queryResults.on('end', function () {
+      done();
+      res.send({success: true});
+    });
+
+  });//end pg connect
+});//end app.post
+
+//delete todo item
+app.post('/deletetodo', urlencodedParser, function (req, res) {
+  console.log('in deletetodo');
+  console.log('req.body', req.body);
+  //create variables from req
+
+  var deleteWhich = req.body.toDelete;
+  console.log('deleteWhich:', deleteWhich);
+  //connect to database
+  pg.connect( connectionString, function (err, client, done) {
+    if (err){
+      console.log(err);
+    }else {
+      console.log('connected to database in delete');
+      var queryResults = client.query('DELETE FROM todolist WHERE todotext= $1', [deleteWhich]);
+
+    }//end else
+    queryResults.on('end', function(){
+    done();
+    return res.send({success: true});
+  });//end queryResults on end
+  });//end pg connect
+});//end app.post
