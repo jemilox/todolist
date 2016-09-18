04fbf5e9003292d432e9d241d5598ba90da66bc9
Lisa Cook <jemilox@gmail.com>
2016-09-17 21:49:17 -0500

    first commit

---
+++ server/app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded( {extended: false});
+var portDecision = process.env.PORT || 3000;
+
+var count = 0;
+
+app.listen( portDecision, function () {
+  console.log("3000 is up!");
+});//end server up
+
+app.get('/', urlencodedParser, function (req, res) {
+  console.log('base url hit');
+  res.sendFile(path.resolve('public/index.html'));
+});
+
+app.use(express.static('public'));
